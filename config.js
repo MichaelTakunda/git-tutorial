5a17fba07542c6a9c95759183238617088ad8cf2
Michael Takunda <michaeltakundamoyo@gmail.com>
2025-09-27 00:26:09 +0200

    Version 1 restored

--- config.js
+++ config.js
@@ -1,3 +1,1 @@
 console.log('Configuration file loaded.')
-console.log('This is the config file.')
-console.log("Version 3.0");
--- src/index.js
+++ src/index.js
@@ -1,3 +1,1 @@
 console.log("This is the config file.");
-console.log("Configuration file loaded.");
-console.log("Version 3.0");
